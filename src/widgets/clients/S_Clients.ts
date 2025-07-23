d09b611f1974f55f40c6707ec8b556b4fad565e0
TanyaDanilovich <tan.danilovich@gmail.com>
2025-07-23 18:24:29 +0300

    start migration

--- src/widgets/clients/S_Clients.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-import {sectionMargin} from '../../app/styles/mixins';
-
-
-export const S_Clients = styled.section<{}>`
-  background-color: ${({theme}) => theme.colors.white};
-  ${sectionMargin}
-`
