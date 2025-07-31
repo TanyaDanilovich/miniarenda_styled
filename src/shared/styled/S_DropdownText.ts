88d483243f1cc28a9dd740342456b653c20ea6eb
TanyaDanilovich <tan.danilovich@gmail.com>
2025-07-31 19:48:17 +0300

    remove styled dropdownfix

--- src/shared/styled/S_DropdownText.ts
+++ src/shared/styled/S_DropdownText.ts
@@ -1,17 +1,24 @@
 import styled from 'styled-components';
 import {plainTransition} from '../../app/styles/mixins.ts';
 
-export const S_DropdownText = styled.div<{ $maxHeight: string, $height: string }>`
-  margin: 0;
 
-  p {
-    padding: 0 1rem 0 3.5rem;
-    overflow: scroll;
+// padding: 0 1rem 0 3.5rem;
+
+export const S_DropdownText = styled.div<{
+    $maxHeight: string,
+    $height: string,
+    $padding?: string
+}>`
+  margin: 0;
+  padding: ${({$padding}) => $padding || '0'};
+  overflow: scroll;
+  
+  p{
     ${plainTransition()}
     max-height: ${({$maxHeight}) => $maxHeight};
     height: ${({$height}) => $height};
-
-
   }
+ 
+
 
 `
