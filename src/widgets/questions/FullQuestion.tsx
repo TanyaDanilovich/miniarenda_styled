1bfdf545386d2ca100c8bb580ee46dab7e02d39f
TanyaDanilovich <tan.danilovich@gmail.com>
2025-09-09 17:57:05 +0300

    remove dom item from faq

--- src/widgets/questions/FullQuestion.tsx
+++ src/widgets/questions/FullQuestion.tsx
@@ -1,9 +1,9 @@
-import styled, {css, useTheme} from 'styled-components';
+import styled, {css} from 'styled-components';
 import {S_Flex} from '../../shared/styled/S_Flex';
-import { plainTransition} from '../../app/styles/mixins';
-import {Question} from './Question';
-import FontAwesomeIcon from '../../shared/ui/icon/FontAwesomeIcon';
+import {plainTransition} from '../../app/styles/mixins';
 import {QuestionDropdownText} from './QuestionDropdownText.tsx';
+import {getResponsiveSize} from '../../shared/utils/getResponsiveSize';
+import {BASE} from '../../shared/constants/constants';
 
 
 type props = {
@@ -15,7 +15,6 @@ type props = {
 }
 
 export const FullQuestion = ({question, answer, toggleQuestionCallback, isOpen}: props) => {
-    const theme = useTheme()
 
     const onToggleQuestion = () => toggleQuestionCallback()
     const textSchemaOrg = {itemProp: "text"}
@@ -27,22 +26,11 @@ export const FullQuestion = ({question, answer, toggleQuestionCallback, isOpen}:
     return (
         <S_FullQuestion $isOpen = {isOpen} itemScope itemProp = "mainEntity" itemType = "https://schema.org/Question">
 
-            <S_Flex $align = {"center"} onClick = {onToggleQuestion}>
-
-                <S_MinusContainer>
-                    <FontAwesomeIcon icon = {'icon-minus'} size = {'1x'} color = {theme.colors.primary}/>
-                    <FontAwesomeIcon icon = {'icon-minus'} size = {'1x'} color = {theme.colors.primary}/>
-                </S_MinusContainer>
-
-
-                <Question text = {question}/>
-
-            </S_Flex>
-
-
+            <S_QuestionHeader onClick = {onToggleQuestion} itemProp = "name" $isOpen = {isOpen} >
+                {question}
+            </S_QuestionHeader>
             <QuestionDropdownText text = {answer} isOpen = {isOpen} schemaOrg = {schemaOrg}
                                   textSchemaOrg = {textSchemaOrg}/>
-
         </S_FullQuestion>);
 };
 
@@ -50,53 +38,50 @@ export const FullQuestion = ({question, answer, toggleQuestionCallback, isOpen}:
 export const S_FullQuestion = styled.div<{ $isOpen: boolean }>`
     position:relative;
     border:1px solid ${({theme}) => theme.colors.gray};
-   
-
-    svg{
-        ${plainTransition()}
-        &:first-child{
-            transform:rotate(90deg);
-        }
-
-        &:last-child{
-            opacity:1;
-        }
-
-        ${({$isOpen}) => $isOpen && css`
-            &:first-child{
-                transform:rotate(180deg);
-                color:${({theme}) => theme.colors.gray};
-            }
-
-            &:last-child{
-                transform:rotate(90deg);
-                opacity:0;
-            }
-        `
-        }
-    }
 
     ${S_Flex}{
-       
         background-color:transparent;
         ${({$isOpen}) => $isOpen && css`
             background-color:${({theme}) => theme.colors.gainsboro};
         `
-        }
+        }`
 
 
-`
+const S_QuestionHeader = styled.button<{ $isOpen: boolean }>`
+    padding:${getResponsiveSize(BASE / 2, BASE * 2)};
+    padding-left:4.5rem;
+    cursor:pointer;
+    flex-grow:1;
+    display:flex;
+    align-items:center;
+    gap:12px;
+    width:100%;
+    background:transparent;
+    border:0;
+    text-align:left;
 
-
-const S_MinusContainer = styled.div<{}>`
-    position:relative;
-    width:1.5em;
-    height:1.5em;
-    margin-left:1rem;
-
-    svg{
+    &:before, &:after{
         position:absolute;
+        inset:1.5rem 1.5rem 1.5rem 1.5rem;
+        display:inline-block;
+        content:" ";
+        width:1rem;
+        height:1rem;
+        background-image:url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="%23fd5d14"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/></svg>');
+        background-position:center;
+        background-repeat:no-repeat;
+        opacity:1;
+        ${plainTransition()};
     }
 
+    &:after{
+        transform:rotate(90deg);
+    }
+
+    ${({$isOpen}) => $isOpen && css`
+        &:after{
+            transform:rotate(0deg);
+        }
+    `};
 
 `
